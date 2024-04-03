c3dc7288b036b559a562c951065c76be48308d71
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-04 00:31:32 +0100

    Add in link to logout

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,13 +1,13 @@
 function ArticlePreview(props) {
   return (
     <div className="Navbar">
-      <div className="h-[10vh] p-5">
+      <div className="flex items-center justify-between h-[10vh] p-5">
         <div className="flex items-center">
           <h1 className='font-serif font-bold text-4xl'>myMedium</h1>
           <span className="h-full mx-5">|</span>
           <p>Hello</p>
         </div>
-        <ion-icon name="log-out-outline"></ion-icon>
+        <a className="text-2xl" href="/"><ion-icon name="log-out-outline"></ion-icon></a>
       </div>
     </div>
   );
