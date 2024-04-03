4091eac489efcea171569bf7239ff24cf0d0d319
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-03 22:59:13 +0100

    Explore page content progress

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,7 +1,29 @@
+import ArticlePreview from "./components/ArticlePreview";
+import Navbar from "./components/Navbar";
+
 function Explore() {
   return (
-    <div className="Explore">
-      EXPLORE
+    <div className="Explore h-screen">
+      <div className="h-screen">
+        <Navbar />
+        <div className="flex h-fit">
+          <div className="flex p-5">
+            <div className="flex w-3/4 gap-5">
+              <div className="w-1/2">
+                <ArticlePreview topStory />
+              </div>
+              <div className="flex flex-col w-1/2 h-full justify-between gap-5">
+                <ArticlePreview />
+                <ArticlePreview />
+                <ArticlePreview />
+                <ArticlePreview />
+              </div>
+            </div>
+            <hr className="bg-black h-full w-px mx-5" />
+            <div className="w-1/4"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
