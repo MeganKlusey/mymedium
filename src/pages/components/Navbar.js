4016c439fde98d10d77ec296eb8f8a916ad7ce38
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-04 00:33:04 +0100

    Add in logo to navbar

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,9 +1,14 @@
+import logo from '../../assets/images/logo.svg';
+
 function ArticlePreview(props) {
   return (
     <div className="Navbar">
       <div className="flex items-center justify-between h-[10vh] p-5">
         <div className="flex items-center">
-          <h1 className='font-serif font-bold text-4xl'>myMedium</h1>
+          <div className='flex items-center justify-center gap-x-1 w-full'>
+            <img className='h-8' src={logo} alt='myMedium logo' />
+            <h1 className='font-serif font-bold text-4xl'>myMedium</h1>
+          </div>
           <span className="h-full mx-5">|</span>
           <p>Hello</p>
         </div>
