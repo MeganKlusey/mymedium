404e4cdad3b118487ce20c83a6b825a086e9de16
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-04 00:03:07 +0100

    Explore page height

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,12 +1,14 @@
 import ArticlePreview from "./components/ArticlePreview";
 import Navbar from "./components/Navbar";
+import Creator from "./components/Creator";
+import Topic from "./components/Topic";
 
 function Explore() {
   return (
     <div className="Explore h-screen">
       <div className="h-screen">
         <Navbar />
-        <div className="flex h-fit">
+        <div className="flex h-[90vh] overflow-hidden">
           <div className="flex p-5">
             <div className="flex w-3/4 gap-5">
               <div className="w-1/2">
@@ -20,7 +22,15 @@ function Explore() {
               </div>
             </div>
             <hr className="bg-black h-full w-px mx-5" />
-            <div className="w-1/4"></div>
+            <div className="w-1/4">
+              <div className="flex flex-col gap-4">
+                <h3 className="uppercase font-bold">Creators</h3>
+                <Creator name="Gabrielle Canon" profile="gabrielle-canon" />
+                <Creator name="Kari Paul" profile="kari-paul" />
+              </div>
+              <h3 className="uppercase font-bold mt-20">Topics</h3>
+              <Topic />
+            </div>
           </div>
         </div>
       </div>
