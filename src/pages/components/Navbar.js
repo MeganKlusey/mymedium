84f1028ef28bb55ca488d129e3413cee87553740
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-03 22:59:02 +0100

    Navbar content and styling progress

---
+++ src/pages/components/Navbar.js
@@ -0,0 +1,15 @@
+function ArticlePreview(props) {
+  return (
+    <div className="Navbar">
+      <div className="px-5 py-2.5">
+        <div className="flex items-center">
+          <h1 className='font-serif font-bold text-4xl'>myMedium</h1>
+          <span className="h-full mx-5">|</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ArticlePreview;
