b3fc6564fe62074d3fbd4b6034394e569dd14230
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-12 21:45:09 +0100

    Update urls

--- src/pages/components/Creator.js
+++ src/pages/components/Creator.js
@@ -3,8 +3,8 @@ function Creator(props) {
     <div className="Creator">
       <div className="flex items-center justify-between">
         <div>
-          <a className="block text-black" href={`/profile/${props.profile}`}>{props.name}</a>
-          <a className="block text-brand-green" href={`/profile/${props.profile}`}>profile/{props.profile}</a>
+          <a className="block text-black" href={`/profiles/${props.profile}`}>{props.name}</a>
+          <a className="block text-brand-green" href={`/profiles/${props.profile}`}>profile/{props.profile}</a>
         </div>
         <button className="text-brand-green border border-brand-green py-2 px-6 rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 duration-200">Follow</button>
       </div>
--- src/pages/components/Topic.js
+++ src/pages/components/Topic.js
@@ -2,7 +2,7 @@ function Topic(props) {
   return (
     <div className="Topic">
       <div className="flex items-center justify-between border-b pb-4">
-        <a className="block text-black" href={`/topic/${props.topic}`}>{props.title}</a>
+        <a className="block text-black" href={`/topics/${props.topic}`}>{props.title}</a>
         <button className="text-brand-green border border-brand-green py-2 px-6 rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 duration-200">Follow</button>
       </div>
     </div>
