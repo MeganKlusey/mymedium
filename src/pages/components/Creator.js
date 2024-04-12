4ea2d05b6eef6e8b1235d6da523ed56991649a5b
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-12 23:54:58 +0100

    Remove url text from Creator

--- src/pages/components/Creator.js
+++ src/pages/components/Creator.js
@@ -1,11 +1,8 @@
 function Creator(props) {
   return (
     <div className="Creator">
-      <div className="flex items-center justify-between">
-        <div>
-          <a className="block text-black" href={`/profiles/${props.profile}`}>{props.name}</a>
-          <a className="block text-brand-green" href={`/profiles/${props.profile}`}>profile/{props.profile}</a>
-        </div>
+      <div className="flex items-center justify-between border-b pb-4">
+        <a className="block text-black" href={`/profiles/${props.profile}`}>{props.name}</a>
         <button className="text-brand-green border border-brand-green py-2 px-6 rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 duration-200">Follow</button>
       </div>
     </div>
