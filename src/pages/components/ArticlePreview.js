db8dc6d1ce40e6812ee434052456edb189b84f77
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-14 18:15:18 +0100

    Line clamps

--- src/pages/components/ArticlePreview.js
+++ src/pages/components/ArticlePreview.js
@@ -3,12 +3,12 @@ function ArticlePreview(props) {
     <div className="ArticlePreview">
       <div className={`flex ${props.topStory ? 'flex-col-reverse' : 'flex-row h-full'}`}>
         <div className={`article-text ${props.topStory ? 'w-full' : 'w-4/5'}`}>
-          <p className="uppercase text-lg">Written by <span className="font-bold">Gabrielle Canon</span></p>
-          <h2 className={`font-extrabold ${props.topStory ? 'text-4xl' : 'text-xl'}`}>Free 
+          <p className="uppercase text-lg line-clamp-1">Written by <span className="font-bold">Gabrielle Canon</span></p>
+          <h2 className={`font-extrabold line-clamp-2 ${props.topStory ? 'text-4xl' : 'text-xl'}`}>Free 
           internet for all not such a bad iea | Brief letters</h2>
           <p className="font-light">2023-12-11 <span className="font-bold">&#8901;</span> 16:19:44 <span className="text-xl align-baseline">&#8902;</span></p>
           {props.topStory &&
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            <p className="line-clamp-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
               consequat</p>
