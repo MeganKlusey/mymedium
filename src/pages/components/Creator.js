bdb9b94e20f5835977ec1d5a3e3d1c757bdd54a4
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-16 20:25:50 +0100

    Add tags to text

--- src/pages/components/Creator.js
+++ src/pages/components/Creator.js
@@ -4,7 +4,9 @@ function Creator(props) {
   return (
     <div className="Creator">
       <div className="flex items-center justify-between border-b pb-4">
-        <NavLink to={`/profiles/${props.profile}`}>{props.name}</NavLink>
+        <NavLink to={`/profiles/${props.profile}`}>
+          <h5 className='text-base'>{props.name}</h5>
+        </NavLink>
         <button className="text-brand-green border border-brand-green py-2 px-6 
         rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 
         duration-200">Follow</button>
--- src/pages/components/Topic.js
+++ src/pages/components/Topic.js
@@ -4,7 +4,9 @@ function Topic(props) {
   return (
     <div className="Topic">
       <div className="flex items-center justify-between border-b pb-4">
-        <NavLink to={`/topics/${props.topic}`}>{props.title}</NavLink>
+        <NavLink to={`/topics/${props.topic}`}>
+          <h5 className='text-base'>{props.title}</h5>
+        </NavLink>
         <button className="text-brand-green border border-brand-green py-2 px-6 
         rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 
         duration-200">Follow</button>
