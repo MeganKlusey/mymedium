69a48c4d7c720060ce9d578fec867c425fb14c51
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-16 22:44:18 +0100

    Navbar responsiveness

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -4,18 +4,18 @@ import logo from '../../assets/images/logo.svg';
 function ArticlePreview(props) {
   return (
     <div className="Navbar">
-      <div className="flex items-center justify-between h-[10vh] p-5">
+      <div className="flex items-center justify-between h-[10vh] p-2.5 md:p-5">
         <div className="flex items-center">
           <NavLink to="/explore">
-            <div className='flex items-center justify-center gap-x-1 w-full'>
-              <img className='h-8' src={logo} alt='myMedium logo' />
-              <h1 className='font-serif font-bold text-4xl'>myMedium</h1>
+            <div className='flex items-center justify-center gap-x-0.5 sm:gap-x-1 w-full'>
+              <img className='h-4 xs:h-8' src={logo} alt='myMedium logo' />
+              <h1 className='font-serif font-bold text-2xl xs:text-4xl'>myMedium</h1>
             </div>
           </NavLink>
           <span className="hidden md:block h-full mx-5">|</span>
           <p className="hidden md:block">Hello</p>
         </div>
-        <div className='flex gap-x-4'>
+        <div className='hidden sm:flex gap-x-4'>
           <NavLink to="/favourites">
             <div className="text-2xl hover:text-brand-green duration-200">
               <ion-icon name="star-outline"></ion-icon>
