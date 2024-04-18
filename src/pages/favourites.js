ab5473ebd80d40c1f9f50b79ed5561ba807fe7a9
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-18 20:41:10 +0100

    Add back button to new pages

--- src/pages/favourites.js
+++ src/pages/favourites.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import BackButton from "./components/BackButton";
 
 function Favourites() {
   return (
     <div className="Favourites">
       <Navbar />
+      <div className="mt-[10vh] lg:mt-0 p-2.5 xs:p-5">
+        <BackButton />
+      </div>
     </div>
   );
 }
