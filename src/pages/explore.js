cdcfee1d011b2a1b97888317d7359e7b1a29845b
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-18 21:36:11 +0100

    Right column responsiveness

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -9,12 +9,12 @@ function Explore() {
       <div className="lg:h-screen">
         <Navbar />
         <div className="flex h-auto lg:h-[90vh] lg:overflow-hidden relative mt-[10vh] lg:mt-0 relative">
-          <div className="flex flex-col lg:flex-row p-2.5 xs:p-5">
+          <div className="flex flex-col lg:flex-row p-2.5 xs:p-5 w-full">
             <div className="flex flex-col md:flex-row w-full lg:w-3/4 gap-8 md:gap-5">
               <div className="w-full md:w-1/2">
                 <ArticlePreview topStory />
               </div>
-              <div className="grid grid-rows-4 w-full md:w-1/2 h-full justify-stretch md:justify-between gap-5 border-t md:border-0 pt-4 md:pt-0">
+              <div className="grid grid-rows-4 w-full md:w-1/2 h-full justify-stretch gap-5 border-t md:border-0 pt-4 md:pt-0">
                 <ArticlePreview />
                 <ArticlePreview />
                 <ArticlePreview />
@@ -22,14 +22,14 @@ function Explore() {
               </div>
             </div>
             <hr className="hidden md:block bg-black h-full w-full lg:w-px my-8 lg:my-0 lg:mx-5" />
-            <div className="w-full lg:w-1/4">
+            <div className="flex flex-col w-full lg:w-1/4">
               <div className="flex flex-col gap-4">
                 <h4 className="uppercase font-bold text-base mt-8 md:mt-0">Creators</h4>
                 <Creator name="Gabrielle Canon" creator="gabrielle-canon" />
                 <Creator name="Kari Paul" creator="kari-paul" />
               </div>
               <div className="flex flex-col gap-4">
-                <h4 className="uppercase font-bold mt-16 lg:mt-20 text-base">Topics</h4>
+                <h4 className="uppercase font-bold mt-16 text-base">Topics</h4>
                 <Topic title="Social Media" topic="social-media" />
                 <Topic title="Coronavirus" topic="coronavirus" />
                 <Topic title="Robots" topic="robots" />
