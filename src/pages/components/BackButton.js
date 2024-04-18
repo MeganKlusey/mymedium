78b71432cbfaf4cf737887a2e9e1613a0f53c8d5
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-04-18 20:40:42 +0100

    Back button content and styling

---
+++ src/pages/components/BackButton.js
@@ -0,0 +1,20 @@
+import {NavLink} from "react-router-dom";
+
+function BackButton(props) {
+  return (
+    <div className="BackButton">
+      <div className="w-fit">
+        <NavLink to="/explore">
+          <button className="group flex items-center gap-2.5 w-fit">
+            <div className="flex duration-200 group-hover:text-brand-green">
+              <ion-icon name="chevron-back-outline"></ion-icon>
+            </div>
+            <p className="uppercase w-fit duration-200 group-hover:text-brand-green">Back</p>
+          </button>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default BackButton;
