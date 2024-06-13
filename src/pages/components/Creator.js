fed80daddd48bc5aced98e4d0ebbf65db61c7232
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-06-13 22:44:02 +0100

    Remove navlinks

--- src/pages/components/Creator.js
+++ src/pages/components/Creator.js
@@ -1,12 +1,8 @@
-import {NavLink} from 'react-router-dom';
-
 function Creator(props) {
   return (
     <div className="Creator">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
-        <NavLink to={`/creators/${props.id.replace(/profile\//g, "")}`}>
-          <h5 className='text-base'>{props.firstName}&nbsp;{props.lastName}</h5>
-        </NavLink>
+        <h5 className='text-base'>{props.firstName}&nbsp;{props.lastName}</h5>
         <button className="text-brand-green border border-brand-green py-2 px-6 
         rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 
         duration-200">Follow</button>
--- src/pages/components/Topic.js
+++ src/pages/components/Topic.js
@@ -1,12 +1,8 @@
-import {NavLink} from 'react-router-dom';
-
 function Topic(props) {
   return (
     <div className="Topic">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
-        <NavLink to={`/topics/${props.topic}`}>
-          <h5 className='text-base'>{props.title}</h5>
-        </NavLink>
+        <h5 className='text-base'>{props.title}</h5>
         <button className="text-brand-green border border-brand-green py-2 px-6 
         rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 
         duration-200">Follow</button>
