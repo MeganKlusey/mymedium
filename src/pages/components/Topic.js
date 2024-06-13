17b2c7ea861f74171fc70a208fc0a6f5bb62d12c
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-06-13 23:57:45 +0100

    Apply line clamp to topic title

--- src/pages/components/Topic.js
+++ src/pages/components/Topic.js
@@ -2,7 +2,7 @@ function Topic(props) {
   return (
     <div className="Topic">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
-        <h5 className='text-base'>{props.title}</h5>
+        <h5 className='text-base line-clamp-1'>{props.title}</h5>
         <button className="text-brand-green border border-brand-green py-2 px-6 
         rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 
         duration-200">Follow</button>
