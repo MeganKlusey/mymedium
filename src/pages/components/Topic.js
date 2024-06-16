8768ddbee59cd64cd6e5d0fcb2e3f7523ff456ed
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-06-16 20:30:45 +0100

    Add favourite topic state and styling

--- src/pages/components/Topic.js
+++ src/pages/components/Topic.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 function Topic(props) {
+  const [favouriteTopic, setFavouriteTopic] = useState(false);
+
   return (
     <div className="Topic">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
         <h5 className='text-base line-clamp-1'>{props.title}</h5>
-        <button className="text-brand-green border border-brand-green py-2 px-6 
-        rounded-md h-fit hover:text-white hover:bg-brand-green hover:opacity-80 
-        duration-200">Follow</button>
+        <button className={`${favouriteTopic ? 'bg-brand-green text-white' : 'text-brand-green hover:bg-brand-green hover:text-white'} border border-brand-green py-2 px-6 
+        rounded-md h-fit hover:opacity-80 
+        duration-200`} onClick={() => setFavouriteTopic(!favouriteTopic)}>{favouriteTopic ? "Following" : "Follow"}</button>
       </div>
     </div>
   );
