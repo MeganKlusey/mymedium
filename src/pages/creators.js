d3d16d7c47eaf004b7888c540c1aa7a001ccfce5
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-06-30 20:40:34 +0100

    List Creators in creators page

--- src/pages/creators.js
+++ src/pages/creators.js
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
 import BackButton from "./components/BackButton";
+import Creator from "./components/Creator";
 
-function Creators() {
+function Creators(props) {
   return (
     <div className="Creators">
       <Navbar />
@@ -9,6 +10,15 @@ function Creators() {
         <div className="flex items-center justify-between">
           <h2 className="text-2xl font-bold">Creators</h2>
           <BackButton />
+        </div>
+        <div className="flex flex-col gap-4 mt-8">
+          {props.data && props.data.filter(creator => creator?.followed).map((creator) => (
+            <div key={creator?.tags[0].id}>
+              <Creator key={creator?.id} firstName={creator?.tags[0]?.firstName} 
+                lastName={creator?.tags[0]?.lastName} id={creator?.tags[0]?.id}
+                data={props.data} setData={props.setData} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
