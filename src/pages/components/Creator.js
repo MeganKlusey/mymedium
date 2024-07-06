3c125f2dda5ece71eb2d9b02ca9bcf737693deb3
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-07-06 22:57:18 +0100

    Format

--- src/pages/components/Creator.js
+++ src/pages/components/Creator.js
@@ -12,11 +12,11 @@ function Creator(props) {
     <div className="Creator">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
         <h5 className='text-base'>{props.firstName}&nbsp;{props.lastName}</h5>
-          <button className={`${currentCreator.followed ? 'bg-brand-green text-white' : 'text-brand-green hover:bg-brand-green hover:text-white'}
-          border border-brand-green py-2 px-6 rounded-md h-fit hover:opacity-80 duration-200`}
-          onClick={handleFollowToggle}>
-            {currentCreator?.followed ? "Following" : "Follow"}
-          </button>
+        <button className={`${currentCreator.followed ? 'bg-brand-green text-white' : 'text-brand-green hover:bg-brand-green hover:text-white'}
+        border border-brand-green py-2 px-6 rounded-md h-fit hover:opacity-80 duration-200`}
+        onClick={handleFollowToggle}>
+          {currentCreator?.followed ? "Following" : "Follow"}
+        </button>
       </div>
     </div>
   );
