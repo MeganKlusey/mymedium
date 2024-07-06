22c1763458ec177e9deae9ed15c8be46f80b3d56
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-07-06 22:51:33 +0100

    Change favourite to follow for topics

--- src/pages/components/Topic.js
+++ src/pages/components/Topic.js
@@ -1,9 +1,9 @@
 function Topic(props) {
   const currentTopic = props.topics.find(topic => topic.id === props.id);
 
-  const handleFavouriteToggle = () => {
+  const handleFollowToggle = () => {
     const updatedTopics = props.topics.map((topic) =>
-      topic.id === props.id ? { ...topic, favourited: !topic.favourited } : topic
+      topic.id === props.id ? { ...topic, followed: !topic.followed } : topic
     );
     props.setTopics(updatedTopics)
   };
@@ -12,10 +12,10 @@ function Topic(props) {
     <div className="Topic">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
         <h5 className='text-base line-clamp-1'>{props.title}</h5>
-        <button className={`${currentTopic?.favourited ? 'bg-brand-green text-white' : 'text-brand-green hover:bg-brand-green hover:text-white'}
+        <button className={`${currentTopic?.followed ? 'bg-brand-green text-white' : 'text-brand-green hover:bg-brand-green hover:text-white'}
         border border-brand-green py-2 px-6 rounded-md h-fit hover:opacity-80 duration-200`}
-        onClick={handleFavouriteToggle}>
-          {currentTopic?.favourited ? "Following" : "Follow"}
+        onClick={handleFollowToggle}>
+          {currentTopic?.followed ? "Following" : "Follow"}
         </button>
       </div>
     </div>
