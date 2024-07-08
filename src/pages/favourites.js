7bfb083a96e75abb8e409618e0743d984b958e3f
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-07-08 20:06:30 +0100

    List favourited articles

--- src/pages/favourites.js
+++ src/pages/favourites.js
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
 import BackButton from "./components/BackButton";
+import ArticlePreview from "./components/ArticlePreview";
 
-function Favourites() {
+function Favourites(props) {
   return (
     <div className="Favourites">
       <Navbar />
@@ -9,6 +10,20 @@ function Favourites() {
         <div className="flex items-center justify-between">
           <h2 className="text-2xl font-bold">Favourites</h2>
           <BackButton />
+        </div>
+        <div className="flex flex-col gap-4 mt-8">
+          {props.data && props.data.filter(data => data.favourited).map((data) => (
+            <div key={data.id}>
+              <ArticlePreview key={data.id} data={props.data} setData={props.setData} 
+                title={data.webTitle} 
+                thumbnail={data.fields.thumbnail} firstName={data.tags[0].firstName} 
+                lastName={data.tags[0].lastName} 
+                webPublicationDate={data.webPublicationDate} id={data.id}  />
+            </div>
+          ))}
+          {props.data.filter(topic => topic.favourited).length === 0 && 
+            <p className="text-center font-bold">No articles favourited.</p>
+          }
         </div>
       </div>
     </div>
