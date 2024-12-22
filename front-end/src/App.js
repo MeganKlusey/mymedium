9a4a80d251e48fec8459da90007b64a5d271479c
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-12-22 23:13:03 +0000

    Debug gh

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
   const apiUrl =
   // process.env.NODE_ENV === 'development'
   // ? 'http://0.0.0.0:8000' : 
-  process.env.REACT_APP_API_URL
+  process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     fetch(`${apiUrl}/data`)
