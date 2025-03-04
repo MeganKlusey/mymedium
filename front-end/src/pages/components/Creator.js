84f47946ac2dbfb3d76c48cf8ecb95f42285379c
MeganKlusey <megan.klusey@hotmail.co.uk>
2025-03-04 16:23:22 +0000

    Fix creator overflow

--- front-end/src/pages/components/Creator.js
+++ front-end/src/pages/components/Creator.js
@@ -12,7 +12,7 @@ function Creator(props) {
   return (
     <div className="Creator">
       <div className="flex items-center justify-between border-b pb-4 gap-2">
-        <h5 className='text-base'>{props.firstName}&nbsp;{props.lastName}</h5>
+        <h5 className='text-base line-clamp-1'>{props.firstName} {props.lastName}</h5>
         <button className={`${currentCreator.followed ? 'bg-brand-green text-white' : 'text-brand-green hover:bg-brand-green hover:text-white'}
         border border-brand-green py-2 px-6 rounded-md h-fit hover:opacity-80 duration-200`}
         onClick={handleFollowToggle}>
